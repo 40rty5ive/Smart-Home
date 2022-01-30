78f8458aea37026d30168b1c27176cbed0b2c53c
Yaroslav <apolisuk065@gmail.com>
2022-01-30 14:01:32 +0200

    Refactoring

--- lib/widgets/home_page/smart_light_widget.dart
+++ lib/widgets/home_page/smart_light_widget.dart
@@ -15,12 +15,142 @@ class SmartLightWidget extends StatefulWidget {
 }
 
 class _SmartLightWidgetState extends State<SmartLightWidget> {
-  bool _powerVal = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          BackArrowWidget(),
+          MainTitleWidget(),
+          ImageOfALampWidget(),
+          PowerSwitchWidget(),
+          ColorSelectorWidget(),
+          ToneGlowWidget(),
+          IntensityWidget(),
+          DraggableScrollableSheet(
+            initialChildSize: 0.2,
+            minChildSize: 0.13,
+            maxChildSize: 0.9,
+            builder: (BuildContext context, ScrollController scrollController) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: Container(
+                            width: 30,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(
+                                color: AppColors.mainGray,
+                                width: 1.5,
+                              ),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class IntensityWidget extends StatefulWidget {
+  const IntensityWidget({Key? key}) : super(key: key);
+
+  @override
+  _IntensityWidgetState createState() => _IntensityWidgetState();
+}
+
+class _IntensityWidgetState extends State<IntensityWidget> {
+  double _sliderValue = 0.23;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 586, 20, 0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Intensity',
+                style: AppTextStyle.mainText,
+              ),
+              Text(
+                '${(_sliderValue * 100).toStringAsFixed(0)} %',
+                style: AppTextStyle.smallMainText,
+              ),
+            ],
+          ),
+          Slider(
+            max: 1,
+            activeColor: AppColors.mainGray,
+            inactiveColor: Colors.white,
+            value: _sliderValue,
+            onChanged: (double value) {
+              setState(() {
+                _sliderValue = value;
+              });
+            },
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  'Off',
+                  style: AppTextStyle.microMainText,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  '100%',
+                  style: AppTextStyle.microMainText,
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ToneGlowWidget extends StatefulWidget {
+  const ToneGlowWidget({Key? key}) : super(key: key);
+
+  @override
+  State<ToneGlowWidget> createState() => _ToneGlowWidgetState();
+}
+
+class _ToneGlowWidgetState extends State<ToneGlowWidget> {
   Color firstButtonColor = AppColors.mainGray;
   Color firstTextColor = Colors.white;
   Color secondButtonColor = Colors.white;
   Color secondTextColor = AppColors.mainGray;
-  double _sliderValue = 0.23;
 
   void _tabOnFirstButton() {
     setState(() {
@@ -42,189 +172,188 @@ class _SmartLightWidgetState extends State<SmartLightWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 469, 20, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 50,
-            ),
-            child: IconButton(
-              onPressed: () => Navigator.of(context).pop(),
-              icon: Icon(Icons.arrow_back),
-            ),
+          Text(
+            'Tone Glow',
+            style: AppTextStyle.mainText,
           ),
-          Container(
-            width: 150,
-            padding: EdgeInsets.fromLTRB(20, 99, 0, 0),
-            child: Text(
-              'Living Room',
-              style: TextStyle(
-                color: AppColors.mainGray,
-                fontSize: 32,
-                fontWeight: FontWeight.w700,
-              ),
-              maxLines: 2,
-            ),
-          ),
-          Container(
-            alignment: Alignment.topRight,
-            child: Image(
-              image: AssetImage(Images.lampOrange),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 239,
-            ),
-            child: Column(children: [
-              Text(
-                'Power',
-                style: AppTextStyle.mainText,
-              ),
-              Transform.scale(
-                scale: 0.8,
-                child: CupertinoSwitch(
-                  onChanged: (bool value) {
-                    setState(() {
-                      _powerVal = value;
-                    });
-                  },
-                  value: _powerVal,
-                  activeColor: AppColors.mainGray,
-                ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Text(
-                'Color',
-                style: AppTextStyle.mainText,
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Image.asset(
-                  Images.changeColor,
-                ),
-                iconSize: 40,
-              )
-            ]),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(20, 456, 20, 0),
-            alignment: Alignment.topLeft,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Tone Glow',
-                  style: AppTextStyle.mainText,
-                ),
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    InkWell(
-                      onTap: _tabOnFirstButton,
-                      child: Container(
-                        height: 32,
-                        width: (MediaQuery.of(context).size.width * 0.5) - 20,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                            color: firstButtonColor,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10.0),
-                              bottomLeft: Radius.circular(10.0),
-                            )),
-                        child: Text(
-                          'Warm',
-                          style: TextStyle(
-                            color: firstTextColor,
-                          ),
-                        ),
-                      ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              InkWell(
+                onTap: _tabOnFirstButton,
+                child: Container(
+                  height: 32,
+                  width: (MediaQuery.of(context).size.width * 0.5) - 20,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: firstButtonColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10.0),
+                        bottomLeft: Radius.circular(10.0),
+                      )),
+                  child: Text(
+                    'Warm',
+                    style: TextStyle(
+                      color: firstTextColor,
                     ),
-                    InkWell(
-                      onTap: _tabOnSecondButton,
-                      child: Container(
-                        clipBehavior: Clip.hardEdge,
-                        height: 32,
-                        width: (MediaQuery.of(context).size.width * 0.5) - 20,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                            color: secondButtonColor,
-                            borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(10.0),
-                              bottomRight: Radius.circular(10.0),
-                            )),
-                        child: Text(
-                          'Cold',
-                          style: TextStyle(
-                            color: secondTextColor,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Container(
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Intensity',
-                            style: AppTextStyle.mainText,
-                          ),
-                          Text(
-                            '${(_sliderValue * 100).toStringAsFixed(0)} %',
-                            style: AppTextStyle.smallMainText,
-                          ),
-                        ],
-                      ),
-                      Slider(
-                        max: 1,
-                        activeColor: AppColors.mainGray,
-                        inactiveColor: Colors.white,
-                        value: _sliderValue,
-                        onChanged: (double value) {
-                          setState(() {
-                            _sliderValue = value;
-                          });
-                        },
-                      ),
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 20),
-                            child: Text(
-                              'Off',
-                              style: AppTextStyle.microMainText,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 20),
-                            child: Text(
-                              '100%',
-                              style: AppTextStyle.microMainText,
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
                   ),
-                )
-              ],
+                ),
+              ),
+              InkWell(
+                onTap: _tabOnSecondButton,
+                child: Container(
+                  clipBehavior: Clip.hardEdge,
+                  height: 32,
+                  width: (MediaQuery.of(context).size.width * 0.5) - 20,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: secondButtonColor,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10.0),
+                        bottomRight: Radius.circular(10.0),
+                      )),
+                  child: Text(
+                    'Cold',
+                    style: TextStyle(
+                      color: secondTextColor,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ColorSelectorWidget extends StatelessWidget {
+  const ColorSelectorWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 344,
+        ),
+        child: Column(children: [
+          Text(
+            'Color',
+            style: AppTextStyle.mainText,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset(
+              Images.changeColor,
+            ),
+            iconSize: 40,
+          ),
+        ]));
+  }
+}
+
+class PowerSwitchWidget extends StatefulWidget {
+  const PowerSwitchWidget({Key? key}) : super(key: key);
+
+  @override
+  _PowerSwitchWidgetState createState() => _PowerSwitchWidgetState();
+}
+
+class _PowerSwitchWidgetState extends State<PowerSwitchWidget> {
+  bool _powerVal = false;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 239,
+      ),
+      child: Column(
+        children: [
+          Text(
+            'Power',
+            style: AppTextStyle.mainText,
+          ),
+          Transform.scale(
+            scale: 0.8,
+            child: CupertinoSwitch(
+              onChanged: (bool value) {
+                setState(() {
+                  _powerVal = value;
+                });
+              },
+              value: _powerVal,
+              activeColor: AppColors.mainGray,
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ImageOfALampWidget extends StatelessWidget {
+  const ImageOfALampWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topRight,
+      child: Image(
+        image: AssetImage(Images.lampOrange),
+      ),
+    );
+  }
+}
+
+class MainTitleWidget extends StatelessWidget {
+  const MainTitleWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      padding: EdgeInsets.fromLTRB(20, 99, 0, 0),
+      child: Text(
+        'Living Room',
+        style: TextStyle(
+          color: AppColors.mainGray,
+          fontSize: 32,
+          fontWeight: FontWeight.w700,
+        ),
+        maxLines: 2,
+      ),
+    );
+  }
+}
+
+class BackArrowWidget extends StatelessWidget {
+  const BackArrowWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 50,
+      ),
+      child: IconButton(
+        onPressed: () => Navigator.of(context).pop(),
+        icon: Icon(Icons.arrow_back),
       ),
     );
   }
